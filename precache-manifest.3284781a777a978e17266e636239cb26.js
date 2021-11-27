f38430ba6843687fec8239a0b1c25be08a51e24b
lecascyril <castagnet.cyril@gmail.com>
2021-11-27 05:25:22 +0100

    Updates

---
+++ precache-manifest.3284781a777a978e17266e636239cb26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24639b35b15262d4f59199bfc67cb2b9",
+    "url": "/boxTest/index.html"
+  },
+  {
+    "revision": "a4215a02d24f861b7a4d",
+    "url": "/boxTest/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8430a0f5a6de933ecf4b",
+    "url": "/boxTest/static/js/2.6a8db0e9.chunk.js"
+  },
+  {
+    "revision": "a4215a02d24f861b7a4d",
+    "url": "/boxTest/static/js/main.c018629d.chunk.js"
+  },
+  {
+    "revision": "6b98a1f24e75b3ef964f",
+    "url": "/boxTest/static/js/runtime-main.406ac6a8.js"
+  }
+]);
